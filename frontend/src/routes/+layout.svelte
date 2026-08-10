57d528071878c590cbba4d829cdcefb23eb0eeec
Code-Barru <antoine.ousselin@protonmail.com>
2026-08-11 01:14:16 +0200

    chore(frontend): frontend init

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import './layout.css';
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<div class="h-screen w-screen bg-slate-700 text-slate-200">
+	{@render children()}
+</div>
